6bb537125860849a6c4265da4d688c41791b9f03
Ravsaheb_Patil <raosaheb95p@gmail.com>
2023-07-23 14:51:38 +0530

    this is the project of retailer location service

---
+++ src/main/java/com/krushikatta/KrushiApplication.java
@@ -0,0 +1,13 @@
+package com.krushikatta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrushiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krushikatta/KrushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krushikatta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrushiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
